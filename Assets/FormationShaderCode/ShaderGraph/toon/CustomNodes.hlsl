a2c707b20f47238bc8dc37a201dbdb9f546adbb1
formateur <formateur@pem.fr>
2025-11-18 11:51:44 +0100

    update

--- Assets/FormationShaderCode/ShaderGraph/toon/CustomNodes.hlsl
+++ Assets/FormationShaderCode/ShaderGraph/toon/CustomNodes.hlsl
@@ -24,3 +24,37 @@ void MainLight_half(out half3 Direction, out half3 Color, out half DistanceAtten
     DistanceAtten = mainLight.distanceAttenuation;
     #endif
 }
+
+void MainLightShadows_float(float3 WorldPos, half4 Shadowmask, out float ShadowAtten){
+    #ifdef SHADERGRAPH_PREVIEW
+    ShadowAtten = 1;
+    #else
+    #if defined(_MAIN_LIGHT_SHADOWS_SCREEN) && !defined(_SURFACE_TYPE_TRANSPARENT)
+    float4 shadowCoord = ComputeScreenPos(TransformWorldToHClip(WorldPos));
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+    ShadowAtten = MainLightShadow(shadowCoord, WorldPos, Shadowmask, _MainLightOcclusionProbes);
+    #endif
+}
+
+void MainLightShadows_float(float3 WorldPos, out float ShadowAtten){
+    MainLightShadows_float(WorldPos, half4(1,1,1,1), ShadowAtten);
+}
+
+void MainLightShadows_half(float3 WorldPos, half4 Shadowmask, out half ShadowAtten){
+    #ifdef SHADERGRAPH_PREVIEW
+    ShadowAtten = 1;
+    #else
+    #if defined(_MAIN_LIGHT_SHADOWS_SCREEN) && !defined(_SURFACE_TYPE_TRANSPARENT)
+    float4 shadowCoord = ComputeScreenPos(TransformWorldToHClip(WorldPos));
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+    ShadowAtten = MainLightShadow(shadowCoord, WorldPos, Shadowmask, _MainLightOcclusionProbes);
+    #endif
+}
+
+void MainLightShadows_half(float3 WorldPos, out half ShadowAtten){
+    MainLightShadows_half(WorldPos, half4(1,1,1,1), ShadowAtten);
+}
